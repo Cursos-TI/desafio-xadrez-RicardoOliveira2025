5c3564c094f6b004d2c4c60d6767f8565b883038
RicardoOliveira2025 <202503392692@alunos.estacio.br>
2025-06-02 01:50:05 +0000

    Movimentotorrebisporainha

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
 int main() {
+    int torre;
+    int bispo = 1;
+    int rainha = 1;
+
     //Movimento da torre
-    for (int i = 0; i < 5; i++) {
+
+    for (int torre = 0; torre < 5; torre++) {
         printf("Mova uma casa pra frente\n");
     }
 
     //Movimento do Bispo
 
-    while (b < 5) {
-        printf("Mover uma casa na diagonal direita\n", b);
-        b++;
-
+    while (bispo <= 5) {
+        printf("Mover uma casa na diagonal direita\n", bispo);
+        bispo++;
     }
 
-
     //Movimento da Rainha
+
+    do {
+
+        printf("Mova uma casa para esquerda\n", rainha);
+        rainha++;
+
+    } while (rainha <= 7);
+
 
     
 
